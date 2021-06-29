bc6f8538f29864dd8e4e315a3ca44668405c1894
amishpopli <amishpopli@yahoo.com>
2021-06-29 13:49:32 -0400

    dask script updated

--- create-dask-clutser.sh
+++ create-dask-clutser.sh
@@ -10,9 +10,9 @@ gcloud dataproc clusters create ${CLUSTER_NAME} \
   --image-version=1.5 \
   --master-machine-type=n1-standard-4 \
   --worker-machine-type=n1-standard-4 \
-  --num-workers 2 \
+  --num-workers 4 \
   --bucket=${BUCKET_NAME} \
   --optional-components=ANACONDA,JUPYTER \
   --enable-component-gateway \
   --metadata 'PIP_PACKAGES=google-cloud-bigquery google-cloud-storage' \
-  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/python/pip-install.sh
+  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/dask/dask.sh
