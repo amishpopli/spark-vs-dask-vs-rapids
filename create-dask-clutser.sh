f1325f25a840f6c0356fc68326820198c49c52ad
amishpopli <amishpopli@yahoo.com>
2021-06-28 20:38:39 -0400

    create cluster job added

---
+++ create-dask-clutser.sh
@@ -0,0 +1,18 @@
+CLUSTER_NAME=explore-dataproc-dask
+REGION=us-central1
+ZONE=us-central1-a
+BUCKET_NAME=explore-gcp-v1
+
+
+gcloud dataproc clusters create ${CLUSTER_NAME} \
+  --region=${REGION} \
+  --zone=${ZONE} \
+  --image-version=1.5 \
+  --master-machine-type=n1-standard-4 \
+  --worker-machine-type=n1-standard-4 \
+  --num-workers 2 \
+  --bucket=${BUCKET_NAME} \
+  --optional-components=ANACONDA,JUPYTER \
+  --enable-component-gateway \
+  --metadata 'PIP_PACKAGES=google-cloud-bigquery google-cloud-storage' \
+  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/python/pip-install.sh
